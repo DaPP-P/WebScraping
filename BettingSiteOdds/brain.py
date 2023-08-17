4d017c83dfb737ebf0f2de16901ab03ee4dd6a64
Daniel Prvanov <prvda311@student.otago.ac.nz>
2023-08-17 21:30:18 +1200

    Minor tweaks

--- BettingSiteOdds/brain.py
+++ BettingSiteOdds/brain.py
@@ -12,6 +12,11 @@ arbitrage_path = "arbitrage.py"
 if platform.system() == "Windows":
     # Use 'start' command on Windows
     subprocess.run(["start",scraper_path], shell=True)
+    print("Scraper Done")
+    subprocess.run(["start",resultChecker_path], shell=True)
+    print("Result Checker Done")
+    print("------------------")
+    subprocess.run(["start",arbitrage_path], shell=True)
 else:
     # Use 'open' command on macOS or Linux
     subprocess.run(["python3",scraper_path])
@@ -19,7 +24,6 @@ else:
     subprocess.run(["python3",resultChecker_path])
     print("Result Checker Done")
     print("------------------")
-    print("ARBITRAGE RESULTS:")
     subprocess.run(["python3",arbitrage_path])
 
 
