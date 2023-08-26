6754e7af14e52ac23c7b9135b30c595dc235a403
Daniel <danielprva@gmail.com>
2023-08-26 16:00:40 +1200

    Done more work on webscraper, works pretty quick for tab, unibet and pointsbet, have errors with resultChecker need to fix.

--- BettingSiteOdds/Scrapers/scraperV2.py
+++ BettingSiteOdds/Scrapers/scraperV2.py
@@ -9,15 +9,50 @@ from bs4 import BeautifulSoup
 # Initial admin. Set up with help from: https://www.youtube.com/watch?v=kpONBQ3muLg
 user_agent = 'Mozilla/5.0 (X11; Linux aarch64; rv:109.0) Gecko/20100101 Firefox/116.0'
 firefox_driver = os.path.join(os.getcwd(), 'Drivers', 'geckodriver')
-firefox_service = Service(firefox_driver)
+
+# Create separate services for each browser instance
+tab_service = Service(firefox_driver, log_output=None)
+pointsbet_service = Service(firefox_driver, log_output=None)
+unibet_service = Service(firefox_driver, log_output=None)
+
 firefox_options = Options()
 firefox_options.set_preference('general.useragent.override', user_agent)
-firefox_options.headless = True
-browser = webdriver.Firefox(service=firefox_service, options=firefox_options)
-browser.get("https://www.tab.co.nz/sport/8/basketball/matches")
+firefox_options.add_argument('-headless')
+
+# Create separate browsers for each website needing to be accessed
+tab_browser = webdriver.Firefox(service=tab_service, options=firefox_options)
+pointsbet_browser = webdriver.Firefox(service=pointsbet_service, options=firefox_options)
+unibet_browser = webdriver.Firefox(service=unibet_service, options=firefox_options)
+
+# Set up paths for the TxtFiles
+script_dir = os.path.dirname(os.path.abspath(__file__))
+txt_files_folder = os.path.join(script_dir, "..", "TxtFiles")
+
+# Set up path for the tab
+tab_file_name = "tabResults.txt"
+tab_file_path = os.path.join(txt_files_folder, tab_file_name)
+
+# Set up path for pointsbet
+pointsbet_file_name = "pointsbetResults.txt"
+pointsbet_file_path = os.path.join(txt_files_folder, pointsbet_file_name)
+
+# Set up path for unibet
+unibet_file_name = "unibetResults.txt"
+unibet_file_path = os.path.join(txt_files_folder, unibet_file_name)
+
+
+# Load TAB website
+tab_browser.get("https://www.tab.co.nz/sport/8/basketball/matches")
+
+# Load PointsBet website
+pointsbet_browser.get("https://pointsbet.com.au/sports/basketball")
+
+# Load Unibet website
+unibet_browser.get("https://www.unibet.com/betting/sports/filter/basketball/all/matches")
+
 time.sleep(20)
 
-def get_odds(url, container_name):
+def get_odds(url, container_name, browser):
     print("Opeing", url)
     #browser.get(url)
     #time.sleep(10)
@@ -41,22 +76,45 @@ def get_odds(url, container_name):
 
 def perform_task():
     # Get odds for the TAB
-    tab_odds = get_odds("https://www.tab.co.nz/sport/8/basketball/matches", "event-list event-list--vertical")
+    tab_odds = get_odds("https://www.tab.co.nz/sport/8/basketball/matches", "event-list event-list--vertical", tab_browser)
     print(tab_odds)
+    upload_odds("tab", tab_odds)
+    print("---------")
+    pointsbet_odds = get_odds("https://pointsbet.com.au/sports/basketball", "f3wis39", pointsbet_browser)
+    print(pointsbet_odds)
+    upload_odds("pointsbet", pointsbet_odds)
+    print("---------")
+    unibet_odds = get_odds("https://www.unibet.com/betting/sports/filter/basketball/all/matches", "_28843", unibet_browser)
+    print(unibet_odds)
+    upload_odds("unibet", unibet_odds)
 
+def upload_odds(website, odds):
+    file_path = tab_file_path if website == "tab" else pointsbet_file_path if website == "pointsbet" else unibet_file_path
+    with open(file_path, "a") as file:
+        if odds:
+            for odd in odds:
+                file.write(str(odd))
+            print("raw", website, "have been scraped")
+        else:
+            print("raw", website, "have NOT been scraped")
+
+i = 0
 while True:
-    i = 0
+
+    print("We are here")
     # Generate a random time interval between 1 and 2 minutes
-    interval = random.randint(60, 120) # seconds random.randint(60, 120)  
+    interval = random.randint(2, 5) # seconds random.randint(60, 120)  
 
     # Wait for the random interval
     time.sleep(interval)
 
     # Perform the task
     i += 1
-    if i < 5:
+    if i < 2:
         print("starting task")
         perform_task()
         print("finished task", i)
     else:
-        browser.quit()
+        tab_browser.quit()
+        pointsbet_browser.quit()
+        break
