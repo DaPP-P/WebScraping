973f47f9b8261d9b2d0501759223aa7d650a9d55
Daniel <danielprva@gmail.com>
2023-11-06 11:38:15 +1300

    test works, time to start proccessing on mac, apart from ladbrokes Tennis but whatever

--- BettingSiteOdds/scraperV2.py
+++ BettingSiteOdds/scraperV2.py
@@ -6,6 +6,7 @@ import selenium.webdriver as webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 from bs4 import BeautifulSoup
+from timeout_decorator import timeout, TimeoutError
 
 # Inital admin for subprocess.
 python_interpreter = 'python3'
@@ -21,15 +22,15 @@ tab_service = Service(firefox_driver, log_output=None)
 pointsbet_service = Service(firefox_driver, log_output=None)
 unibet_service = Service(firefox_driver, log_output=None)
 ladbrokes_service1 = Service(firefox_driver, log_output=None)
-ladbrokes_service2 = Service(firefox_driver, log_output=None)
+#ladbrokes_service2 = Service(firefox_driver, log_output=None)
 ladbrokes_service3 = Service(firefox_driver, log_output=None)
 #tonybet_service = Service(firefox_driver, log_output= None)
 
 # Create separate service for each TENNIS browser instance
-#tab_tennis_service = Service(firefox_driver, log_output=None)
-#pointsbet_tennis_service = Service(firefox_driver, log_output=None)
-#unibet_tennis_service = Service(firefox_driver, log_output=None)
-#ladbrokes_tennis_service = Service(firefox_driver, log_output=None)
+tab_tennis_service = Service(firefox_driver, log_output=None)
+pointsbet_tennis_service = Service(firefox_driver, log_output=None)
+unibet_tennis_service = Service(firefox_driver, log_output=None)
+ladbrokes_tennis_service = Service(firefox_driver, log_output=None)
 
 
 # Set the settings 
@@ -43,15 +44,15 @@ tab_browser = webdriver.Firefox(service=tab_service, options=firefox_options)
 pointsbet_browser = webdriver.Firefox(service=pointsbet_service, options=firefox_options)
 unibet_browser = webdriver.Firefox(service=unibet_service, options=firefox_options)
 ladbrokes_browser1 = webdriver.Firefox(service=ladbrokes_service1, options=firefox_options)
-ladbrokes_browser2 = webdriver.Firefox(service=ladbrokes_service2, options=firefox_options)
+#ladbrokes_browser2 = webdriver.Firefox(service=ladbrokes_service2, options=firefox_options)
 ladbrokes_browser3 = webdriver.Firefox(service=ladbrokes_service3, options=firefox_options)
 #tonybet_browser = webdriver.Firefox(service=tonybet_service, options=firefox_options)
 
 # Create separate browsers for each TENNIS website needing to be accessed
-#tab_tennis_browser = webdriver.Firefox(service=tab_tennis_service, options=firefox_options)
-#pointsbet_tennis_browser = webdriver.Firefox(service=pointsbet_tennis_service, options=firefox_options)
-#unibet_tennis_browser = webdriver.Firefox(service=unibet_tennis_service, options=firefox_options)
-#ladbrokes_tennis_browser = webdriver.Firefox(service=ladbrokes_tennis_service, options=firefox_options)
+tab_tennis_browser = webdriver.Firefox(service=tab_tennis_service, options=firefox_options)
+pointsbet_tennis_browser = webdriver.Firefox(service=pointsbet_tennis_service, options=firefox_options)
+unibet_tennis_browser = webdriver.Firefox(service=unibet_tennis_service, options=firefox_options)
+ladbrokes_tennis_browser = webdriver.Firefox(service=ladbrokes_tennis_service, options=firefox_options)
 
 
 # Set up paths for the TxtFiles for BASKETBALL
@@ -66,22 +67,22 @@ tennis_txt_files_folder = os.path.join(script_dir, "TxtFilesTennis")
 # Set up paths for the Tab
 tab_file_name = "tabResults.txt"
 tab_file_path = os.path.join(txt_files_folder, tab_file_name)
-#tab_tennis_file_path = os.path.join(tennis_txt_files_folder, tab_file_name)
+tab_tennis_file_path = os.path.join(tennis_txt_files_folder, tab_file_name)
 
 # Set up path for Pointsbet
 pointsbet_file_name = "pointsbetResults.txt"
 pointsbet_file_path = os.path.join(txt_files_folder, pointsbet_file_name)
-#pointsbet_tennis_file_path = os.path.join(tennis_txt_files_folder, pointsbet_file_name)
+pointsbet_tennis_file_path = os.path.join(tennis_txt_files_folder, pointsbet_file_name)
 
 # Set up path for Unibet
 unibet_file_name = "unibetResults.txt"
 unibet_file_path = os.path.join(txt_files_folder, unibet_file_name)
-#unibet_tennis_file_path = os.path.join(tennis_txt_files_folder, unibet_file_name)
+unibet_tennis_file_path = os.path.join(tennis_txt_files_folder, unibet_file_name)
 
 # Set up path for Ladbrokes
 ladbrokes_file_name = "ladbrokesResults.txt"
 ladbrokes_file_path = os.path.join(txt_files_folder, ladbrokes_file_name)
-#ladbrokes_tennis_file_path = os.path.join(tennis_txt_files_folder, ladbrokes_file_name)
+ladbrokes_tennis_file_path = os.path.join(tennis_txt_files_folder, ladbrokes_file_name)
 
 # Set up path for Tonybets
 tonybet_file_name = "tonybetResults.txt"
@@ -89,28 +90,56 @@ tonybet_file_path = os.path.join(txt_files_folder, tonybet_file_name)
 
 # Load TAB website
 tab_browser.get("https://www.tab.co.nz/sport/8/basketball/matches")
-#tab_tennis_browser.get("https://www.tab.co.nz/sport/37/tennis/matches")
+tab_tennis_browser.get("https://www.tab.co.nz/sport/37/tennis/matches")
+print("Loaded TAB")
 
 # Load PointsBet website
 pointsbet_browser.get("https://pointsbet.com.au/sports/basketball")
-#pointsbet_tennis_browser.get("https://pointsbet.com.au/sports/tennis")
+pointsbet_tennis_browser.get("https://pointsbet.com.au/sports/tennis")
+print("Loaded Pointsbet")
+
+# Load Labrokes website
+@timeout(300)
+def load_ladbrokes_browser1():
+    ladbrokes_browser1.get("https://www.ladbrokes.com.au/sports/basketball/usa")
+    print("ladbrokes usa done")
+
+@timeout(300)
+def load_ladbrokes_browser3():
+    ladbrokes_browser3.get("https://www.ladbrokes.com.au/sports/basketball/australia")
+    print("ladbrokes australia done")
+
+@timeout(300)
+def load_ladbrokes_tennis():
+    ladbrokes_tennis_browser.get("https://www.ladbrokes.com.au/sports/tennis")
+    print("Loaded Ladbrokes")
+
+try:
+    load_ladbrokes_browser1()
+except TimeoutError:
+    print("ladbrokes usa did not load")
+try:
+    load_ladbrokes_browser3()
+except TimeoutError:
+    print("ladbrokes australia did not load")
+try:
+    load_ladbrokes_tennis()
+except TimeoutError:
+    print("loadbrokes tennis did not load")
 
 # Load Unibet website
 unibet_browser.get("https://www.unibet.com/betting/sports/filter/basketball/all/matches")
-#unibet_tennis_browser.get("https://www.unibet.com/betting/sports/filter/tennis/all/matches")
+unibet_tennis_browser.get("https://www.unibet.com/betting/sports/filter/tennis/all/matches")
+print("Loaded Unibet")
 
-# Load Labrokes website
-ladbrokes_browser1.get("https://www.ladbrokes.com.au/sports/basketball/usa")
-ladbrokes_browser2.get("https://www.ladbrokes.com.au/sports/basketball/international")
-ladbrokes_browser3.get("https://www.ladbrokes.com.au/sports/basketball/australia")
-#ladbrokes_tennis_browser.get("https://www.ladbrokes.com.au/sports/tennis")
+
 
 # Load TonyBet website
 #tonybet_browser.get("https://tonybet.com/nz/prematch/basketball")
 
 # Gives time for the browsers to be loaded
-time.sleep(90)
-
+time.sleep(30)
+print("Done Loading")
 
 # Method for scraping the odds from the website. @input: url, the websites url; container_name, the name of the container
 # that needs to be scraped; browser, the name of the browser that was set up above.
@@ -144,7 +173,7 @@ def perform_task():
     global choice
     
     if choice == True:
-        #choice = False
+        choice = False
 
         # Gets odds for basketball scores from the TAB
         tab_odds = get_odds("https://www.tab.co.nz/sport/8/basketball/matches", "event-list event-list--vertical", tab_browser)
@@ -166,7 +195,8 @@ def perform_task():
 
         # Gets odds for basketball scores from Ladbrokes
         ladbrokes_odds1 = get_odds("https://www.ladbrokes.com.au/sports/basketball/usa", "sports-market-primary__prices-inner", ladbrokes_browser1)
-        ladbrokes_odds2 = get_odds("https://www.ladbrokes.com.au/sports/basketball/international", "sports-market-primary__prices-inner", ladbrokes_browser2)
+        #ladbrokes_odds2 = get_odds("https://www.ladbrokes.com.au/sports/basketball/international", "sports-market-primary__prices-inner", ladbrokes_browser2)
+        ladbrokes_odds2 = []
         ladbrokes_odds3 = get_odds("https://www.ladbrokes.com.au/sports/basketball/australia", "sports-market-primary__prices-inner", ladbrokes_browser3)
         ladbrokes_upload_odds(ladbrokes_odds1, ladbrokes_odds2, ladbrokes_odds3)
         print("---------")
@@ -199,8 +229,7 @@ def perform_task():
         ladbrokes_odds = get_odds("https://www.ladbrokes.com.au/sports/tennis", "sports-market-primary__prices-inner", ladbrokes_tennis_browser)
         print(ladbrokes_odds)
         upload_odds("ladbrokes", ladbrokes_odds, "Tennis")
-        print("("---------")")
-
+        print("---------")
 
 
 # Method for importing the scraped odds to the desired txt file.
@@ -287,9 +316,13 @@ while i < 2 :
 # Once the program has been run i amount of times it must close the browsers.
 else:
     tab_browser.quit()
+    tab_tennis_browser.quit()
     pointsbet_browser.quit()
+    pointsbet_tennis_browser.quit()
     unibet_browser.quit()
+    unibet_tennis_browser.quit()
     ladbrokes_browser1.quit()
-    ladbrokes_browser2.quit()
+    #ladbrokes_browser2.quit()
     ladbrokes_browser3.quit()
+    ladbrokes_tennis_browser.quit()
     #tonybet_browser.quit()
